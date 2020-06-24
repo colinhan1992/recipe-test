22fc83d0469ce7dbd0d81d696786998408758dd6
ColinHan <mr.colinhan@gmail.com>
2020-06-23 19:23:47 -0500

    adding recipe dialog

--- src/components/recipe.vue
+++ src/components/recipe.vue
@@ -1,26 +1,62 @@
 
   
 <template>
-  
+  <v-card> 
+      <v-card-title class="justify-space-between"> 
+        {{ recipe.title }}
+        <v-btn v-if="$vuetify.breakpoint.xsOnly" icon @click="$emit('close')"><v-icon>close</v-icon></v-btn>
+      </v-card-title>
+      
+    
+    <v-card-text>
+      <v-img
+        class="white--text align-end mb-3"
+        height="200px"
+        :src="require('../../public' + recipe.images.medium)"
+      >
+    </v-img>
+      <p>
+      <span class="font-weight-medium">{{ recipe.description }}</span> <br>
+      <span class="pr-3"><v-icon>group</v-icon> <strong>{{ recipe.servings }}</strong> Servings</span>
+      <span class="pr-3"><v-icon>schedule</v-icon> <strong>{{ recipe.servings }}</strong> min Preptime</span>
+      <span class="pr-3"><v-icon>access_alarm</v-icon> <strong>{{ recipe.servings }}</strong> min Cooktime</span>
+      </p>
+
+      <h3>Ingrediants</h3>
+      <ul>
+        <li v-for="item in recipe.ingredients" :key="item.uuid">
+           <strong>{{ item.amount }}</strong> {{ item.measurement }} {{ item.name }} 
+           <span v-for="(special, index) in getSpecials(item)" :key="special.uuid" class="pb-3">
+             <span v-if="index === 0"><br></span>
+              <v-chip small> {{ special.type.toUpperCase()}}: {{ special.title }} </v-chip> <br>
+              <span class="font-italic caption"> {{ special.text }}</span>
+           </span>
+        </li>
+      </ul>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 
   export default {
     props: {
-      recipes: Array,
-      specials: Array,
+      recipe: { type: Object },
+      specials: { type: Array },
     },
     data: () => ({
       
     }),
 
     methods: {
-      
+      getSpecials(ingredient) {
+        console.log(this.specials.filter(s => s.ingredientId === ingredient.uuid))
+        return this.specials.filter(s => s.ingredientId === ingredient.uuid)
+      },
     },
 
     mounted() {
-      this.load()
+
     }
   }
 </script>
